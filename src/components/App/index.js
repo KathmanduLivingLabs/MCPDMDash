ff5c48df90caca89b9ded934a54f7d3542c53489
Aakash Sigdel <aakash.sigdel@gmail.com>
2015-06-17 15:20:50 +0545

    un done my screw up

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,10 +1,38 @@
 var React = require('react');
+var Map = require('../Map');
+var EthnicChart = require('../EthnicChart');
+var NavBar = require('../NavBar');
+
+require('./style.scss');
 
 export default class App extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			selectedDistrict: null
+		};
+	}
+
+	componentDidUpdate() {
+		console.log(this.state.selectedDistrict);
+	}
+
+	setSelectedDistrict(selectedDistrict) {
+		this.setState({
+			selectedDistrict: selectedDistrict
+		});
+	}
+
 	render() {
 		return(
 			<div className="container">
-			hello	
+				<NavBar />
+				<div className="map-container">
+					<Map setSelectedDistrict={this.setSelectedDistrict.bind(this)} />
+				</div>
+				<div className="chart-container">
+					<EthnicChart selectedDistrict={this.state.selectedDistrict} />
+				</div>
 			</div>
 		);
 	}
