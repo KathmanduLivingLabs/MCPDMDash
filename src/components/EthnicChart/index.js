6dffb7703e501076935736d93a0d1414ecc86520
Aakash Sigdel <aakash.sigdel@gmail.com>
2015-09-21 10:25:40 +0545

    added actual data; still remainaing needs and food data

--- src/components/EthnicChart/index.js
+++ src/components/EthnicChart/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 
+var barData = require('../../data/realData.json');
+
 require('./style.scss');
 
 export default class EthnicChart extends React.Component {
+	constructor() {
+		this.chartData = [];
+	}
+
 	closeChart(e) {
 		document.getElementById("ethnic_chart").style.display = 'none';
 	}
+
 	render() {
 		return(
 			<div id="ethnic_chart" className="ethnic-chart">
