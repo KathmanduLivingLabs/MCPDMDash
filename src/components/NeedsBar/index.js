d25e5d6ae5235fa1478fa9777eeefa852a9c958e
Aakash Sigdel <aakash.sigdel@gmail.com>
2015-09-30 13:20:35 +0545

    pratik dai ko naramro footer

--- src/components/NeedsBar/index.js
+++ src/components/NeedsBar/index.js
@@ -56,6 +56,12 @@ export default class NeedsBar extends React.Component {
 				'Medical Care', 'Supplies of Temporary Shelter', 'Tents', 'Other Assistance'],
 			series: [data[2]]
 		};
+		this.chartNeedsData[3] = {
+			labels: ['Additional Urgent Items not Present in the Kit', 'Shelter Construction Materials', 
+				'Food', 'Clothing', 'Medical Supplies', 'Agricultural Inputs', 'Basic Household Items',
+				'Large Household Items', 'Hygine Products', 'Educational Materials'],
+			series: [data[3]]
+		};
 	}
 
 	makePercentCircles(chart, valueAxis, textMarginX, textMarginY, horizBar) {
@@ -109,9 +115,12 @@ export default class NeedsBar extends React.Component {
 		var chartNeedsMet = new Chartist.Bar('.needs-met', this.chartNeedsData[0], options);
 		var chartNeedsUnmet = new Chartist.Bar('.needs-unmet', this.chartNeedsData[1], options1_2);
 		var chartAssistance = new Chartist.Bar('.assistance', this.chartNeedsData[2], options_2);
+		var chartBreakdown = new Chartist.Bar('.needs-breakdown', this.chartNeedsData[3], options_2);
+
 		this.makePercentCircles(chartNeedsMet, 'y', 18, 5, false);
 		this.makePercentCircles(chartNeedsUnmet, 'y', 18, 5, false);
 		this.makePercentCircles(chartAssistance, 'y', 18, 5, false);
+		this.makePercentCircles(chartBreakdown, 'y', 18, 5, false);
 	}
 
 	render() {
@@ -165,6 +174,17 @@ export default class NeedsBar extends React.Component {
 				Math.round((Number(barData.aggregate.assistance[4]) / total_assistance) * 10000) / 100,
 				Math.round((Number(barData.aggregate.assistance[5]) / total_assistance) * 10000) / 100,
 				Math.round((Number(barData.aggregate.assistance[6]) / total_assistance) * 10000) / 100,
+			], [
+				Number(barData.aggregate.breakdown[0]),
+				Number(barData.aggregate.breakdown[1]),
+				Number(barData.aggregate.breakdown[2]),
+				Number(barData.aggregate.breakdown[3]),
+				Number(barData.aggregate.breakdown[4]),
+				Number(barData.aggregate.breakdown[5]),
+				Number(barData.aggregate.breakdown[6]),
+				Number(barData.aggregate.breakdown[7]),
+				Number(barData.aggregate.breakdown[8]),
+				Number(barData.aggregate.breakdown[9])
 			]]
 		);
 		return(
@@ -176,6 +196,11 @@ export default class NeedsBar extends React.Component {
 				<div className="needs-unmet">
 					<span className="chart-title-needs">Respondets who said not all urgently needed items were included</span>
 				</div>
+				<div className="spacer"></div>
+				<div className="needs-breakdown">
+					<span className="chart-heading title-margin">Urgently Needed Items Not Included in the Kit</span>
+				</div>
+				<div className="spacer"></div>
 				<div className="assistance">
 					<span className="chart-heading title-margin">Types of Assistance Received</span>
 				</div>
